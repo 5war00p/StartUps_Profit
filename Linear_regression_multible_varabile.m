3507d2763bdb0ebff717b70f70f885530dffc66b
Vajrala Venkateswarlu <59252985+venkyvajrala@users.noreply.github.com>
2020-07-03 12:19:56 +0530

    Create Linear_regression_multible_varabile.m

---
+++ Linear_regression_multible_varabile.m
@@ -0,0 +1,7 @@
+dat=importdata('profit1.txt',',',1);
+R_Dspend=dat.data(:,1);
+Administration=dat.data(:,2);
+Marketing=dat.data(:,3);
+Spend=dat.data(:,4);
+State=dat.data(:,5);
+Profit=dat.data(:,6);
