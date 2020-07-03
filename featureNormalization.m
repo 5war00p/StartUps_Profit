fd7e23a52da6a899be1fd25d6e216d8da652af8c
Vajrala Venkateswarlu <59252985+venkyvajrala@users.noreply.github.com>
2020-07-03 14:30:16 +0530

    Update featureNormalization.m

--- featureNormalization.m
+++ featureNormalization.m
@@ -1,9 +1,9 @@
-function X = featureNormalization(X)
+function [X_norm,mu,sigma] = featureNormalization(X)
     % FEATURENORMALIZATION Summary of this function goes here
     % Normalizes all features in X and return same X
     % Detailed explanation goes here
     
  mu=mean(X);
  Std=std(X);
- X=rdivide(minus(X,mu),Std);
+ X_norm=rdivide(minus(X,mu),Std);
 end
