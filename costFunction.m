6c9e60245c7cd9b3981da56c480c460fd88c76d3
5war00p <57614947+5war00p@users.noreply.github.com>
2020-07-03 16:57:16 +0530

    Update costFunction.m

--- costFunction.m
+++ costFunction.m
@@ -1,6 +1,6 @@
 function [J,grad] =costFunction(X,y,theta,lambda);
     m=length(y);
-    X=[ones(m,1) X];
+    %X = [ones(m,1) X];
     h=X*theta;
     J= (1/(2*m))*(sum((h-y).^2));
     grad=(1/m)*((h-y)'*X)';
