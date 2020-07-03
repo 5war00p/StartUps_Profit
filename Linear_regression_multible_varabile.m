a3487b933138819c5f310d2b1fb9fe7c9326262a
Vajrala Venkateswarlu <59252985+venkyvajrala@users.noreply.github.com>
2020-07-03 14:53:03 +0530

    Update Linear_regression_multible_varabile.m

--- Linear_regression_multible_varabile.m
+++ Linear_regression_multible_varabile.m
@@ -1,2 +1,7 @@
 [X,y]=loadData();
 [X,mue,sigma]=featureNormalization(X);
+theta=zeros(size(X,2)+1,1);
+lambda=0;
+[J,grad]=costFunction(X,y,theta,lambda);
+options=optimset('MaxIter',50,'GradObj','on');
+[theta,cost] =fminunc(@(t) costFunction(X,y,theta,lambda),theta,options);
