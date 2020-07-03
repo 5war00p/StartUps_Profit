02a10984605a0f6a7b2b6ed8e46b31bbb25a3aa9
5war00p <57614947+5war00p@users.noreply.github.com>
2020-07-03 16:56:58 +0530

    Update Linear_regression_multible_varabile.m

--- Linear_regression_multible_varabile.m
+++ Linear_regression_multible_varabile.m
@@ -1,17 +1,10 @@
 [X,y]=loadData();
-[X,mean,std]=Normalize(X);
-[y,mean,std]=Normalize(y);
+[X,mue,sigma]=featureNormalization(X);
 theta=zeros(size(X,2)+1,1);
-num_iter=3000;
-alpha=0.01;
 lambda=0;
-%[J,grad]=costFunction(X,y,theta,lambda,num_iter);
-[theta,j_history] =gradient(X,y,theta,alpha,num_iter,lambda);
-%options=optimset('MaxIter',50,'GradObj','on');
+alpha = 0.01;
+num_iterations = 3000;
+%[J,grad]=costFunction(X,y,theta);
+%options=optimset('MaxIter',1500,'GradObj','on');
 %[theta,cost] =fminunc(@(t) costFunction(X,y,theta,lambda),theta,options);
-plot(1:num_iter,j_history);
-m=length(y);
-X1=[ones(m,1) X];
-compare=[X1*theta-y];
-disp(j_history(end-1:end,:));
-
+[theta,j_history] = gradient(X,ynorm,theta,alpha,num_iterations);
