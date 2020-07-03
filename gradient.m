68474b942908ebd618301457d9cbc3d58616aadc
5war00p <57614947+5war00p@users.noreply.github.com>
2020-07-03 16:58:28 +0530

    Update gradient.m

--- gradient.m
+++ gradient.m
@@ -1,3 +1,4 @@
+%{
 function [theta,j_history] =gradient(X,y,theta,alpha,num_iter,lambda)
     
     m=length(y);
@@ -11,4 +12,14 @@ function [theta,j_history] =gradient(X,y,theta,alpha,num_iter,lambda)
     
     
     
+end
+%}
+function [theta,j_history] = gradient(X,y,theta,alpha,num_iterations,lambda)
+    j_history= ones(num_iterations,1);
+    m=length(y);
+    X = [ones(m,1) X];
+    for i=1:num_iterations
+        theta=theta-((alpha/m)*(((X*theta)-y)'*X))';
+        j_history(i) = costFunction(X,y,theta,lambda);
+    end 
 end
