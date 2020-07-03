9936ef41aa08101921d628dcf4cc6abf4c71eacb
5war00p <57614947+5war00p@users.noreply.github.com>
2020-07-03 12:34:38 +0530

    State Updated

--- Linear_regression_multible_varabile.m
+++ Linear_regression_multible_varabile.m
@@ -3,5 +3,5 @@ R_Dspend=str2double(dat.textdata(:,1));
 Administration=str2double(dat.textdata(:,2));
 Marketing=str2double(dat.textdata(:,3));
 Spend=str2double(dat.textdata(:,4));
-State=str2double(dat.textdata(:,5));
+State=dat.textdata(:,5);
 Profit=dat.data();
