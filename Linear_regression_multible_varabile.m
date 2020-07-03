f2cd9291bffd9a1d6b8d005adaecef280bcd3629
Vajrala Venkateswarlu <59252985+venkyvajrala@users.noreply.github.com>
2020-07-03 12:30:53 +0530

    Update Linear_regression_multible_varabile.m

--- Linear_regression_multible_varabile.m
+++ Linear_regression_multible_varabile.m
@@ -1,7 +1,7 @@
 dat=importdata('profit1.txt',',',1);
-R_Dspend=dat.data(:,1);
-Administration=dat.data(:,2);
-Marketing=dat.data(:,3);
-Spend=dat.data(:,4);
-State=dat.data(:,5);
-Profit=dat.data(:,6);
+R_Dspend=str2double(dat.textdata(:,1));
+Administration=str2double(dat.textdata(:,2));
+Marketing=str2double(dat.textdata(:,3));
+Spend=str2double(dat.textdata(:,4));
+State=str2double(dat.textdata(:,5));
+Profit=dat.data();
