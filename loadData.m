8ceeaecb1288616615852134b54a4ea63312fb33
Vajrala Venkateswarlu <59252985+venkyvajrala@users.noreply.github.com>
2020-07-03 14:02:02 +0530

    Update loadData.m

--- loadData.m
+++ loadData.m
@@ -1,15 +1,18 @@
-dat=importdata('profit.txt',',',1);
-RDSpend = str2double(dat.textdata(2:end,1));
-AdminSpend=str2double(dat.textdata(2:end,2));
-MarketingSpend=str2double(dat.textdata(2:end,3));
-SpendStateStrings = dat.textdata(2:end,4);
+function [X,y]=loadData()
 
-SpendState = zeros(size(SpendStateStrings));
-SpendState(find(cellstr(SpendStateStrings) == "California")) = 1;
-SpendState(find(cellstr(SpendStateStrings) == "Florida")) = 2;
-SpendState(find(cellstr(SpendStateStrings) == "New York")) = 3;
+  dat=importdata('profit.txt',',',1);
+  RDSpend = str2double(dat.textdata(2:end,1));
+  AdminSpend=str2double(dat.textdata(2:end,2));
+  MarketingSpend=str2double(dat.textdata(2:end,3));
+  SpendStateStrings = dat.textdata(2:end,4);
 
-Profit = dat.data();
+  SpendState = zeros(size(SpendStateStrings));
+  SpendState(find(cellstr(SpendStateStrings) == "California")) = 1;
+  SpendState(find(cellstr(SpendStateStrings) == "Florida")) = 2;
+  SpendState(find(cellstr(SpendStateStrings) == "New York")) = 3;
 
-X = [RDSpend AdminSpend MarketingSpend SpendState];
-y = Profit;
+  Profit = dat.data();
+
+  X = [RDSpend AdminSpend MarketingSpend SpendState];
+  y = Profit;
+  
